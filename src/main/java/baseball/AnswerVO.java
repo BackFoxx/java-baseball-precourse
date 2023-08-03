932defd8ad6051c602648d09d5e987064c35a6d6
JinseonYe <aatwe24@gmail.com>
2023-08-03 23:40:04 +0900

    [Refactor] QuestionVO, AnswerVO를 배열이 아닌 list로 변경

--- src/main/java/baseball/AnswerVO.java
+++ src/main/java/baseball/AnswerVO.java
@@ -1,36 +1,44 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class AnswerVO {
-	private final String[] answerStrings;
+	private final ArrayList<Integer> answerArray;
 
 	public AnswerVO(String answer) {
-		this.answerStrings = answer.split("");
+		this.answerArray = Arrays.stream(answer.split(""))
+			.mapToInt(Integer::parseInt)
+			.boxed().collect(Collectors.toCollection(ArrayList::new));
 		wrongDigitException();
 		containsZeroException();
 	}
 
-	//문자배열 answerStrings을 숫자배열 answerArray로 변환
-	public int[] answerToAnswerArray() {
-		int[] answerArray = new int[answerStrings.length];
-		for (int i = 0; i < answerStrings.length; i++) {
-			answerArray[i] = Integer.parseInt(answerStrings[i]);
+	//answer 크기 반환
+	public int getAnswerDigits() {
+		return answerArray.size();
+	}
+
+	//answer 인덱스에 따른 값 반환
+	public int getAnswerDigitAt(int index) {
+		if (index < 0 || index >= answerArray.size()) {
+			throw new IndexOutOfBoundsException("유효하지 않은 인덱스입니다.");
 		}
-		return answerArray;
+		return answerArray.get(index);
 	}
 
 	//3자리가 아닌 수를 입력하면 예외 발생
 	private void wrongDigitException() {
-		if (answerStrings.length !=3) {
+		if (answerArray.size() !=3) {
 			throw new IllegalArgumentException("3자리 수를 입력해주세요.");
 		}
 	}
 
 	//답변에 0이 포함되면 예외 발생
 	private void containsZeroException() {
-		for (String answer : answerStrings) {
-			if (answer.contains("0")) {
-				throw new IllegalArgumentException("답변은 0을 포함할 수 없습니다.");
-			}
+		if (answerArray.contains(0)) {
+			throw new IllegalArgumentException("답변은 0을 포함할 수 없습니다.");
 		}
 	}
 }
--- src/main/java/baseball/QuestionVO.java
+++ src/main/java/baseball/QuestionVO.java
@@ -1,33 +1,33 @@
 package baseball;
 
+import java.util.ArrayList;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class QuestionVO {
-	private final int[] question;
+	private final ArrayList<Integer> question = new ArrayList<>();
 
 	private static final int startInclusive = 1;
 	private static final int endInclusive = 9;
 
 	public QuestionVO() {
-		question = new int[3];
-		question[0] = Randoms.pickNumberInRange(startInclusive, endInclusive);
-		question[1] = Randoms.pickNumberInRange(startInclusive, endInclusive);
-		question[2] = Randoms.pickNumberInRange(startInclusive, endInclusive);
+		question.add(Randoms.pickNumberInRange(startInclusive, endInclusive));
+		question.add(Randoms.pickNumberInRange(startInclusive, endInclusive));
+		question.add(Randoms.pickNumberInRange(startInclusive, endInclusive));
 	}
 
 	//문제와 답변의 일치 정도에 따른 힌트 출력
 	public int[] compareTo(AnswerVO answerVO) {
-		int[] answerArray = answerVO.answerToAnswerArray();
 		int strike = 0;
 		int ball = 0;
 
-		for (int i = 0; i < question.length; i++) {
-			int j = (i + 1) % answerArray.length;
-			int k = (i + 2) % answerArray.length;
+		for (int i = 0; i < question.size(); i++) {
+			int j = (i + 1) % answerVO.getAnswerDigits();
+			int k = (i + 2) % answerVO.getAnswerDigits();
 
-			if (question[i] == answerArray[i]) {
+			if (question.get(i).equals(answerVO.getAnswerDigitAt(i))) {
 				strike++;
-			} else if (question[i] == answerArray[j] || question[i] == answerArray[k]) {
+			} else if (question.get(i).equals(answerVO.getAnswerDigitAt(j)) || question.get(i).equals(answerVO.getAnswerDigitAt(k))) {
 				ball++;
 			}
 		}
